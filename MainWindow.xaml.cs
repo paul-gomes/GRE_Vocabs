dc51e22aecf63c579aae940029079a0e1e77d59d
Paul Gomes <gomesap29@gmail.com>
2019-07-31 13:14:54 -0500

    js added to remove dom elements

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,12 +24,32 @@ namespace GRE_Vocabs
             Cef.Initialize(settings);
 
             // Create a browser component
-            chromeBrowser = new ChromiumWebBrowser("https://www.vocabulary.com/dictionary/affable#wordPage");
+            chromeBrowser = new ChromiumWebBrowser("https://www.vocabulary.com/dictionary/affable");
+
+            chromeBrowser.FrameLoadEnd += (sender, args) =>
+            {
+                //Wait for the MainFrame to finish loading
+                if (args.Frame.IsMain)
+                {
+                    args.Frame.ExecuteJavaScriptAsync("var item = document.getElementById('dictionary-upper-ad-right'); item.parentNode.removeChild(item);" +
+                                                      "document.querySelector('#dictionary-lower-ad').style.display = 'none';" +
+                                                      "document.querySelector('.leaderboard-ad').style.display = 'none';" +
+                                                      "document.querySelector('.page-header').style.display = 'none';" +
+                                                      "document.querySelector('.fixed-tray').style.display = 'none';" +
+                                                      "document.querySelector('#dictionaryNav').style.display = 'none'; " +
+                                                      "document.querySelector('.signup-tout').style.display = 'none'; " +
+                                                      "document.querySelector('.page-footer').style.display = 'none'") ;
+                }
+            };
+
 
             // Add it to the form and fill it to the form window.
             vocabView.Children.Add(chromeBrowser);
 
         }
+
+
+
 
         private void vocabulary_click(object sender, RoutedEventArgs e)
         {
